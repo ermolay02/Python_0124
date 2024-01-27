edcc003a446dc0acd6e030ba02dca7527f6b2ab4
ANDREY SEMENOV <ermolay02@gmail.com>
2024-01-27 14:40:09 +0500

    тест2

--- 280124.py
+++ 280124.py
@@ -1,1 +1,6 @@
-print("Hello!")
+print("Привет! Кто ты?")
+Name = input()
+print("Так значит, ты - " + Name)
+print("А как у тебя дела?")
+Answer = input()
+print("Я рад, что у тебя дела " + Answer);
