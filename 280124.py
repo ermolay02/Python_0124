9f8f2743fb34224b37772b639a15234ab258fe53
ANDREY SEMENOV <ermolay02@gmail.com>
2024-01-27 14:42:53 +0500

    тест пока

--- 280124.py
+++ 280124.py
@@ -3,4 +3,4 @@ Name = input()
 print("Так значит, ты - " + Name)
 print("А как у тебя дела?")
 Answer = input()
-print("Я рад, что у тебя дела " + Answer);
+print("Я рад, что у тебя дела " + Answer, "Пока, друг!");
